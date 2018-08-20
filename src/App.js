4456a39bb904cbf415ccd868795c01a6b8f98143
Debraj Ray <debraj1990@gmail.com>
2018-08-20 18:53:37 +0530

    Changes in App.js and index.js to write logic for problem statement # 1: Create a stateful parent controller with 2 stateless child components. Pass props and handlers from parent to child.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,61 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+// 1.	Create a stateful parent controller with 2 stateless child components. Pass props and handlers from parent to child.
+//Stateful parent component "ParentCompAverageCalc"
+class ParentCompAverageCalc extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numIncrease: 3,
+      numDecrease: 8
+    };
+  }
+  // Handler for child component "ChildComp1"
+  childComp1IncrHandler = () => {
+    this.setState({
+      numIncrease: this.state.numIncrease+1
+    });
+  }
+  // Handler for child component "ChildComp2"
+  childComp2DecrHandler = () => {
+    this.setState({
+      numDecrease: this.state.numDecrease-1
+    });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Excercise - Solved By Er. Debraj Ray</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        1]	Create a stateful parent controller with 2 stateless child components. Pass props and handlers from parent to child.
         </p>
+        {/* Props "numIncrease" and handler "childComp1IncrHandler" passed from parent component "ParentCompAverageCalc" to child component "ChildComp1" */}
+        <ChildComp1 onClick={this.childComp1IncrHandler} incrValue={this.state.numIncrease} />
+        {/* Props "numDecrease" and handler "childComp2DecrHandler" passed from parent component "ParentCompAverageCalc" to child component "ChildComp2" */}
+        <ChildComp2 onClick={this.childComp2DecrHandler} decrValue={this.state.numDecrease} />
+        <p>Average of {this.state.numIncrease} and {this.state.numDecrease} is <b>{(this.state.numIncrease + this.state.numDecrease)/2}</b></p>
       </div>
     );
   }
 }
 
-export default App;
+//stateless child component "ChildComp1"
+const ChildComp1 = ({onClick, incrValue}) => (
+  <div>
+    <button onClick={onClick}>+</button>
+    The numeric value#1 received from ParentComp is <b>{incrValue}</b> 
+  </div>
+)
+//stateless child component "ChildComp2"
+const ChildComp2 = ({onClick, decrValue}) => (
+  <div>
+    <button onClick={onClick}>-</button>
+    The numeric value#2 received from ParentComp is <b>{decrValue}</b> 
+  </div>
+)
+
+export default ParentCompAverageCalc;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ParentCompAverageCalc from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ParentCompAverageCalc />, document.getElementById('root'));
 registerServiceWorker();
